0cb90be84c96e63b7d58a2ac83cdf3a24b5991d6
BaeSuJeong <doya60@gmail.com>
2024-11-10 20:59:54 +0900

    메인화면 레이아웃 작업

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Main msg ="hello" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './pages/main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './css/reset.css';
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
+
 </style>
